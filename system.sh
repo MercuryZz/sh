d50c106692e15453eb31abac22875c9897a3e4ca
MercuryZz <52125806+MercuryZz@users.noreply.github.com>
2025-04-01 17:02:58 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,3 +1,24 @@
+#!/usr/bin/env bash
+#
+# ===== 全局变量 =====
+# -> 字体颜色定义 (彩色输出)
+Font_Black="\033[30m"
+Font_Red="\033[31m"
+Font_Green="\033[32m"
+Font_Yellow="\033[33m"
+Font_Blue="\033[34m"
+Font_Purple="\033[35m"
+Font_SkyBlue="\033[36m"
+Font_White="\033[37m"
+Font_Suffix="\033[0m"
+# -> 消息提示定义 (消息前缀)
+Msg_Info="${Font_Blue}[INFO]${Font_Suffix}"
+Msg_Warning="${Font_Yellow}[WARN]${Font_Suffix}"
+Msg_Debug="${Font_Yellow}[DEBUG]${Font_Suffix}"
+Msg_Error="${Font_Red}[ERROR]${Font_Suffix}"
+Msg_Success="${Font_Green}[SUCCESS]${Font_Suffix}"
+Msg_Fail="${Font_Red}[FAIL]${Font_Suffix}"
+Msg_Time="$(date +"[%Y/%m/%d %T]")"
 #
 # ===========================================================================
 # -> 系统信息模块 (Entrypoint) -> 执行
